a47713cc9661e1c1ffecc687925efbe539c9b3e0
Cimabio <cimabiosefaz@gmail.com>
2021-11-04 15:14:34 -0300

    create project

---
+++ src/main/java/com/cimabio/ControleMvcApplication.java
@@ -0,0 +1,13 @@
+package com.cimabio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cimabio/ControleMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cimabio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
